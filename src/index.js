4fd3952765cb7e0966188ad6767d74e16988da87
Jai singh <jaibhandari804@gmail.com>
2023-04-26 15:39:24 +0530

    tooltip added

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ const queryClient = new QueryClient({
 
 
 // if (process.env.NODE_ENV === 'development') {
-axios.defaults.baseURL =  process.env.REACT_APP_ENDPOINT;
+// axios.defaults.baseURL =  process.env.REACT_APP_ENDPOINT;
+axios.defaults.baseURL =  'https://app.organaise.com';
 // } else if (process.env.NODE_ENV === 'production') {
 //   axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
 // }
